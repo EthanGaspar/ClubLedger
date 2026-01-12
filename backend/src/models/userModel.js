127ec1897ff5bafed86295254b191e87f216af37
Ethan Gaspar <131616920+EthanGaspar@users.noreply.github.com>
2026-01-11 17:54:18 -0800

    Implemented field validation for signup

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema(
     {
@@ -18,6 +19,18 @@ const userSchema = new mongoose.Schema(
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
+    //validation
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid")
+    }
+    //min length 12, 1 lowercase, 1 uppercase, 1 number, 1 symbol
+    if (!validator.isStrongPassword(password, { minLength: 12 })) {
+        throw Error("Password not strong enough")
+    }
+    
     const emailExists = await this.findOne({ email })
     //no duplicates
     if (emailExists) {
